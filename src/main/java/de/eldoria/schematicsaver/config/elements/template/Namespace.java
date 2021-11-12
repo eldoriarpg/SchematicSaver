1d3c3b381d236b8638dd47bb78b326f14cfe77bf
Florian Fülling <46890129+RainbowDashLabs@users.noreply.github.com>
2021-11-12 03:14:48 +0100

    Add license

--- src/main/java/de/eldoria/schematicsaver/config/elements/template/Namespace.java
+++ src/main/java/de/eldoria/schematicsaver/config/elements/template/Namespace.java
@@ -1,3 +1,9 @@
+/*
+ *     SPDX-License-Identifier: AGPL-3.0-only
+ *
+ *     Copyright (C) 2021 EldoriaRPG Team and Contributor
+ */
+
 package de.eldoria.schematicsaver.config.elements.template;
 
 import de.eldoria.eldoutilities.serialization.SerializationUtil;
